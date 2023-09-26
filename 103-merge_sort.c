3b22913cf4cbdda41c8dd64bc781502a79beabb5
GentlePhilip <aphilipnyarko@gmail.com>
2023-09-25 18:09:28 -0700

    C - Sorting algorithms & Big O

---
+++ 103-merge_sort.c
@@ -0,0 +1,88 @@
+#include "sort.h"
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+
+/**
+ * merge_subarr - Put an integer subarray in order.
+ * @subarr: a subarray of an array of ordered integers.
+ *
+ * @buff: a buffer to keep the set-up subarray.
+ * @front: The array's first element's index.
+ *
+ * @mid: The array's central index.
+ * @back:The array's back index..
+ */
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
+}
+
+/**
+ * merge_sort_recursive - Implement the merge sort algorithm using recursion.
+ * @subarr: A subarray of an array of integers to arranged.
+ * @buff: A buffer to store the arranged result.
+ *
+ * @front: The front index of the subarray.
+ * @back: The back index of the subarray.
+ */
+
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+{
+	size_t mid;
+
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+		merge_sort_recursive(subarr, buff, front, mid);
+		merge_sort_recursive(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
+	}
+}
+
+/**
+ * merge_sort - Use the merge sort algorithm to order an array of
+ *              numbers in ascending order.
+ * @array: An array of integers.
+ *
+ * @size: The size of the array.
+ * Description: The top-down merge sort technique is implemented.
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	int *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	merge_sort_recursive(array, buff, 0, size);
+
+	free(buff);
+}
